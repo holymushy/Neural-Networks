d7f02d1560c9efe900e8153dcaa9853f97bbdf0a
Oscar Leung <oscarleung1@gmail.com>
2017-12-12 19:42:07 -0800

    Created Skeleton stubs

---
+++ src/neuralNetwork.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+/**
+ * @author Oscar Leung 
+ *
+ */
+public class neuralNetwork {
+
+	/**
+	 * 
+	 */
+	public neuralNetwork() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Initialize the Neural Network
+	 */
+	public static void initialize()	{
+		
+	}
+	/**
+	 * Trains the Neural Network
+	 */
+	public static void train() {
+		
+	}
+	/**
+	 * Query the Neural NetWork
+	 */
+	public static void query() {
+		
+	}
+}
