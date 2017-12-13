e47be3751683505536512872be3eb50617905dd2
Oscar Leung <oscarleung1@gmail.com>
2017-12-12 19:34:22 -0800

    Created Java Stubs

---
+++ src/Neural_Network.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Oscar Leung 
+ *
+ */
+public class Neural_Network {
+
+	/**
+	 * 
+	 */
+	public Neural_Network() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
