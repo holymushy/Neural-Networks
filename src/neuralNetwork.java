340574bc5f110670087dbd827151ad8e633f62e3
Oscar Leung <oscarleung1@gmail.com>
2017-12-18 12:38:52 -0800

    Initilize complete weights

--- src/neuralNetwork.java
+++ src/neuralNetwork.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * 
  */
@@ -6,39 +8,65 @@
  * @author Oscar Leung 
  *
  */
-public class neuralNetwork {
-
+public class neuralNetwork{
+	private static double inodes,hnodes,onodes,lr;
+	// The weightRange is determine by the amount of nodes 
+	final static double weightRange = 1/Math.sqrt(3); //.577 if you didn't know
+	private static double weights;
 	/**
 	 * 
 	 */
 	public neuralNetwork() {
-		// TODO Auto-generated constructor stub
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		initialize(0,0,0,0);
+		System.out.println("The input nodes are " + inodes);
+		System.out.println("The hidden nodes are " + hnodes);
+		System.out.println("The output nodes are " + onodes);
+		System.out.println("The learning rate is " + onodes);
+		System.out.println("The random weights are " + weights);
 
 	}
 
 	/**
 	 * Initialize the Neural Network
 	 */
-	public static void initialize()	{
-		
+	public static void initialize(int inputNode, int hiddenNodes, int outputNodes, int learningRate)	{
+		// Set # of nodes in each input, hidden, and output layers
+		inodes = inputNode;
+		hnodes = hiddenNodes;
+		onodes = outputNodes;
+		// learning rate
+		lr = learningRate;
+		// random weights
+		Random r = new Random();
+		double random = new Random().nextDouble();
+		weights = -weightRange + (random * (weightRange - -(weightRange)));
+
 	}
 	/**
 	 * Trains the Neural Network
 	 */
 	public static void train() {
-		
+
 	}
-	/**
+	/**	
 	 * Query the Neural NetWork
 	 */
 	public static void query() {
-		
+
 	}
+
+	public static double dotProduct(double[] a, double[] b) {
+		double sum = 0;
+		for (int i = 0; i < a.length; i++) {
+			sum += a[i] * b[i];}
+		return sum;
+	}
+
+
 }
